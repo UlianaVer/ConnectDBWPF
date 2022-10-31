4c44f2f6753cd3f3c3fb7b7e72d3450f3c5d33f2
Верещагина Ульяна Николаевна <Vereshchagina.U.N@corp.kp11.ru>
2022-10-31 15:03:44 +0300

    First commit

---
+++ DBConnectionsWPFApp/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBConnectionsWPFApp
+{
+    internal class DBConnect
+    {
+        public static VereshchaginaEntities entities;
+    }
+}
---
+++ DBConnectionsWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DBConnectionsWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int IdUser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            DBConnect.entities = new VereshchaginaEntities();
+
+            refresh_grid();
+        }
+
+        private void DataGridRow_Selected(object sender, RoutedEventArgs e)
+        {
+            var dgrow = (DataGridRow)sender;
+            var context = dgrow.DataContext as User;
+
+            IdUser = context.Id;
+            tbID.Text = context.Id.ToString();
+            tbEmail.Text = context.Email;
+            tbPassword.Text = context.Password;
+        }
+
+       
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var user = DBConnect.entities.User.FirstOrDefault(x => x.Id == IdUser);
+
+            
+            user.Email = tbEmail.Text;
+            user.Password = tbPassword.Text;
+
+            DBConnect.entities.SaveChanges();
+
+            refresh_grid();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var user = DBConnect.entities.User.FirstOrDefault(x => x.Id == IdUser);
+            DBConnect.entities.User.Remove(user);
+
+            DBConnect.entities.SaveChanges();
+
+            refresh_grid();
+        }
+        private void refresh_grid()
+        {
+            dataGrid.ItemsSource = DBConnect.entities.User.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+
+            user.Username = tbUsername.Text;
+            user.Email = tbNewEmail.Text;
+            user.Password = tbNewPassword.Text;
+            user.Birthdate = datePickerBD.SelectedDate;
+            user.IdRole = Convert.ToInt32(tbNewRole.Text);
+
+            DBConnect.entities.User.Add(user);
+            DBConnect.entities.SaveChanges();
+
+            refresh_grid();
+        }
+    }
+}
